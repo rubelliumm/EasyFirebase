93cf8c7006625738929c643e4a4daabfb725a1eb
rubelliumm <ahmedrubel935@gmail.com>
2024-11-15 11:12:43 +0600

    version 2.0

--- EasyFirebase/fields.py
+++ EasyFirebase/fields.py
@@ -1,10 +1,39 @@
 from typing import Any
 from django.db import models
+from django.db.models.fields.files import FieldFile
+from django.db.models.fields.files import ImageFieldFile
+from django.core.files.storage import default_storage
+from django.db.models.signals import pre_delete
+from django.dispatch import receiver
 
 
-class FirebaseImageField(models.ImageField):
-    def __init__(self, *args: Any, **kwargs: Any):
+class _FirebaseFieldFile(FieldFile):
+
+    @property
+    def url(self):
+        return self.storage.url(self.name)
+
+
+class FirebaseFileField(models.FileField):
+    """Free File Hosting base Class"""
+
+    attr_class = _FirebaseFieldFile
+
+    def __init__(self, *args: Any, **kwargs: Any) -> None:
+        kwargs["storage"] = kwargs.get("storage", default_storage)
         super().__init__(*args, **kwargs)
+
+    def deconstruct(self):
+        """
+        Custom deconstruct method to ensure that the storage backend is not serialized in migrations.
+        """
+        name, path, args, kwargs = super().deconstruct()
+
+        # Remove 'storage' from kwargs to avoid serialization issues
+        if "storage" in kwargs:
+            del kwargs["storage"]
+
+        return name, path, args, kwargs
 
     def to_python(self, value):
         if value is not None:
@@ -12,7 +41,57 @@ class FirebaseImageField(models.ImageField):
         return super().to_python(value)
 
     def pre_save(self, model_instance, add):
-        setattr(
-            model_instance, self.attname, None
-        )  # None for not saving the image file.
+        file_changed = False
+
+        # Retrieve the current value from the database if it's not a new instance
+        if not add and model_instance.pk:
+            existing_instance = (
+                model_instance.__class__.objects.filter(pk=model_instance.pk)
+                .only(self.attname)
+                .first()
+            )
+
+            if existing_instance:
+                value_before_save = getattr(existing_instance, self.attname, None)
+                value_after_save = getattr(model_instance, self.attname, None)
+                file_changed = value_before_save != value_after_save and bool(
+                    value_before_save
+                )
+                # print(file_changed)
+
+                if file_changed:
+                    # Delete the old file
+                    field = existing_instance._meta.get_field(self.name)
+                    field_file = getattr(existing_instance, self.name, None)
+                    field.storage.delete(str(field_file))  # type: ignore
+            else:
+                raise ValueError(
+                    "object not found. debug at fields.py in method pre_save"
+                )
+        else:
+            pass
         return super().pre_save(model_instance, add)
+
+    @receiver(pre_delete)
+    def delete_image_on_model_delete(instance, **kwargs):
+        for field in instance._meta.fields:  # type: ignore
+            if isinstance(field, FirebaseFileField):
+                firebase_image_field_file = getattr(instance, field.name, None)
+                if (
+                    firebase_image_field_file is not None
+                    and firebase_image_field_file.name != ""
+                ):
+                    try:
+                        field.storage.delete(str(firebase_image_field_file.name))
+                    except Exception as e:
+                        print(
+                            f"[!!!] Warning! Can't delete the file in source. Exception: {e}\nobject:{instance}.fileld: {field}"
+                        )
+
+
+class FirebaseImageFieldFile(_FirebaseFieldFile, ImageFieldFile):
+    pass
+
+
+class FirebaseImageField(models.ImageField, FirebaseFileField):
+    attr_class = FirebaseImageFieldFile
---
+++ EasyFirebase/storage.py
@@ -0,0 +1,70 @@
+import os
+from django.core.files.storage import Storage
+from django.core.files.storage import Storage
+from django.conf import settings
+
+import firebase_admin
+from firebase_admin import credentials, storage
+
+# Initialize Firebase only once
+
+if not firebase_admin._apps:
+    cred = credentials.Certificate(settings.FIREBASE_CREDENTIALS_FILE)
+    firebase_admin.initialize_app(
+        cred, {"storageBucket": settings.FIREBASE_CONFIG["storageBucket"]}
+    )
+
+
+class FirebaseStorage(Storage):
+
+    def _get_bucket(self):
+        """Helper method to get Firebase storage bucket."""
+        return storage.bucket()
+
+    def _save(self, name, content):
+        """Upload a file to Firebase and return its name."""
+        default_folder = getattr(settings, "FIREBASE_DEFAULT_UPLOAD_DIR", None)
+        if default_folder:
+            name = os.path.join(default_folder, name)
+        else:
+            # adding project name as default dir.
+            project_name = getattr(settings, "BASE_DIR", None)
+            if project_name:
+                name = os.path.join(project_name, name)
+            else:
+                name = os.path.join("FreeDjangoFirebase/", name)
+        bucket = self._get_bucket()
+        blob = bucket.blob(name)
+        blob.upload_from_file(content, content_type=content.content_type)
+        blob.make_public()  # Make the file publicly accessible
+        return name
+
+    def url(self, name):
+        """Return the public URL of the file."""
+        bucket = self._get_bucket()
+        blob = bucket.blob(name)
+        return blob.public_url if blob.exists() else None
+
+    def exists(self, name):
+        """Check if the file exists in Firebase storage."""
+        bucket = self._get_bucket()
+        blob = bucket.blob(name)
+        return blob.exists()
+
+    def delete(self, name):
+        """Delete a file from Firebase storage."""
+        bucket = self._get_bucket()
+        blob = bucket.blob(name)
+        blob.delete()
+
+    def open(self, name, mode="rb"):
+        """Open the file; Firebase does not support direct file retrieval, so raise an error."""
+        raise NotImplementedError(
+            "Direct file opening is not supported by FirebaseStorage.Instead use the 'url' method to get download link of the file."
+        )
+
+    def get_available_name(self, name, max_length=None):
+        """Return the name of the file, ensuring it’s unique."""
+        return (
+            name  # Firebase automatically overwrites existing files with the same name
+        )
